eb4af1a39550e379e784e95bd5dcadd8cf8fae7c
Miquel de Domingo <miquelddg@gmail.com>
2025-12-14 08:48:53 +0100

    feat(web): add web app

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import './layout.css'
+
+    import favicon from '$lib/assets/favicon.svg'
+
+    const { children } = $props()
+</script>
+
+<svelte:head><link href={favicon} rel="icon" /></svelte:head>
+{@render children()}
